82fe2372d92b2533f049736d357fba97742f4b19
박종찬 <chanii8@naver.com>
2022-08-23 01:02:19 +0900

    [생활코딩] React 2022년 개정판 - 7. state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
+function Header(props) {
+    return (
+        <header>
+            <h1>
+                <a
+                    href="/"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        props.onChangeMode();
+                    }}
+                >
+                    {props.title}
+                </a>
+            </h1>
+        </header>
+    );
+}
+function Nav(props) {
+    const lis = props.topic.map((li) => {
+        return (
+            <li key={li.id}>
+                <a
+                    href={"/read/" + li.id}
+                    id={li.id}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        props.onChangeMode(e.target.id);
+                    }}
+                >
+                    {li.title}
+                </a>
+            </li>
+        );
+    });
+    return (
+        <nav>
+            <ol>{lis}</ol>
+        </nav>
+    );
+}
+function Article(props) {
+    return (
+        <article>
+            <h2>{props.title}</h2>
+            {props.body}
+        </article>
+    );
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [mode, setMode] = useState("WELCOME");
+    const [id, setId] = useState(null);
+    const topics = [
+        { id: "html", title: "html", body: "html is ..." },
+        { id: "css", title: "css", body: "css is ..." },
+        { id: "js", title: "js", body: "js is ..." },
+    ];
+    let contents = null;
+    if (mode === "WELCOME") {
+        contents = <Article title="Welcome" body="Hello, WEB" />;
+    } else if (mode === "READ") {
+        const item = topics.find((item) => {
+            return item.id === id;
+        });
+        contents = <Article title={item.title} body={item.body} />;
+    }
+    return (
+        <div>
+            <Header
+                title="WEB"
+                onChangeMode={() => {
+                    setMode("WELCOME");
+                }}
+            />
+            <Nav
+                topic={topics}
+                onChangeMode={(id) => {
+                    setMode("READ");
+                    setId(id);
+                }}
+            />
+            {contents}
+        </div>
+    );
 }
 
 export default App;
